6d65d57d875485de3106368c2bb5faf21c1b247d
Seran Sivananthamoorthy <5sivas01@solent.ac.uk>
2026-09-17 16:21:58 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_PATH || '/',
-}));
+  base: '/my-fuel-price-monitor/',
+});
